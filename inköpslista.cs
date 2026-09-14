3a65addfaadc482e228623a59115a350a0eeec2e
Chiminii <aidoanchau@gmail.com>
2026-09-15 01:24:46 +0200

    gjorde så att det loopar nu,gjorde också att den skriver ut inköpslistan med korrekt index, vara och pris,

--- inköpslista.cs
+++ inköpslista.cs
@@ -4,34 +4,49 @@ static void Groceries()
     List<string> items = [];
     List<int> prices = [];
 
-    // Ask what item to put into the list
-    Console.WriteLine("Input an item for your grocery list: ");
-    // Read the input value
-    string readItem = Console.ReadLine()!;
     int readPrice;
-    int count = 1;
 
-    if (string.IsNullOrWhiteSpace(readItem))
+    while (true)
     {
-        Console.WriteLine("Could not register, input a new item again: ");
-    }
-    else
-    {
-        items.Add(readItem);
-        Console.WriteLine($"{count++}. {readItem} has been added to the grocery list.");
+        // Ask what item to put into the list
+        Console.WriteLine("Input an item for your grocery list: ");
+        // Read the input value
+        string readItem = Console.ReadLine()!;
+        if (string.IsNullOrWhiteSpace(readItem))
+        {
+            Console.WriteLine("Could not register, input a new item again: ");
+            continue;
+        }
+        else
+        {
+            items.Add(readItem);
+            // Console.WriteLine($"{count++}. {readItem} has been added to the grocery list.");
 
-        //Ask about the price
-        Console.WriteLine($"Input a price for the {readItem}: ");
-        if (int.TryParse(Console.ReadLine(), out readPrice))
+        }
+        while (true)
         {
-            prices.Add(readPrice);
-            Console.WriteLine($"It costs {readPrice} kr");
-        }else
+            //Ask about the price
+            Console.WriteLine($"Input a price for the {readItem}: ");
+            if (int.TryParse(Console.ReadLine(), out readPrice))
+            {
+                prices.Add(readPrice);
+                // Console.WriteLine($"It costs {readPrice} kr");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Could not register, input a new price again: ");
+            }
+        }
+        Console.Clear();
+
+        Console.WriteLine("\nGrocery List");
+        // Print out the whole list
+        for (int i = 0; i < items.Count; i++)
         {
-            Console.WriteLine("Could not register, input a new price again: ");
+            // i + 1 to make the list print out start value 1 instead of zero
+            Console.WriteLine($"{i + 1}. {items[i]} - {prices[i]} kr");
         }
     }
-
-
 }
 Groceries();
