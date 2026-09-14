7bab50c6da8cbbf384985a76451c1be921752e70
Chiminii <aidoanchau@gmail.com>
2026-09-15 00:22:52 +0200

    La till count-numrering i listan, provade att göra så att man kan skriva priser

--- inköpslista.cs
+++ inköpslista.cs
@@ -8,6 +8,8 @@ static void Groceries()
     Console.WriteLine("Input an item for your grocery list: ");
     // Read the input value
     string readItem = Console.ReadLine()!;
+    int readPrice;
+    int count = 1;
 
     if (string.IsNullOrWhiteSpace(readItem))
     {
@@ -15,8 +17,21 @@ static void Groceries()
     }
     else
     {
-        items.Add(readItem); 
-        Console.WriteLine($"1. {readItem} has been added to the grocery list.");
+        items.Add(readItem);
+        Console.WriteLine($"{count++}. {readItem} has been added to the grocery list.");
+
+        //Ask about the price
+        Console.WriteLine($"Input a price for the {readItem}: ");
+        if (int.TryParse(Console.ReadLine(), out readPrice))
+        {
+            prices.Add(readPrice);
+            Console.WriteLine($"It costs {readPrice} kr");
+        }else
+        {
+            Console.WriteLine("Could not register, input a new price again: ");
+        }
     }
+
+
 }
 Groceries();
