b80459b2a65c40f71801f4453514630978043835
Chiminii <aidoanchau@gmail.com>
2026-09-15 12:15:41 +0200

    la till beräkning av den totala summan av inköpslistan + städade upp koden

--- inköpslista.cs
+++ inköpslista.cs
@@ -3,19 +3,23 @@ static void Groceries()
     // Creating lists to keep track of the items and their prices
     List<string> items = [];
     List<int> prices = [];
-
     int readPrice;
 
     while (true)
     {
         Console.Clear();
         Console.WriteLine("\nGrocery List");
+        int totalSum = 0;
         // Print out the whole list
         for (int i = 0; i < items.Count; i++)
         {
+            // calculate total cost
+            totalSum += prices[i];
             // i + 1 to make the list print out start value 1 instead of zero
             Console.WriteLine($"{i + 1}. {items[i]} - {prices[i]} kr");
         }
+        Console.WriteLine($"Totalt: {totalSum} kr");
+
         // Ask what item to put into the list
         Console.WriteLine("\nInput an item for your grocery list or a number to remove an item: ");
         // Read the input value
@@ -38,16 +42,12 @@ static void Groceries()
             }
             else
             {
-                Console.WriteLine("That number does not exist on the list.");
+                Console.WriteLine("That number does not exist on the list. Press Enter to try again.");
+                Console.ReadLine();
                 continue;
             }
         }
-        else
-        {
-            //items.Add(readItem);
-            // Console.WriteLine($"{count++}. {readItem} has been added to the grocery list.");
 
-        }
         while (true)
         {
             //Ask about the price
@@ -56,12 +56,11 @@ static void Groceries()
             {
                 items.Add(readItem);
                 prices.Add(readPrice);
-                // Console.WriteLine($"It costs {readPrice} kr");
                 break;
             }
             else
             {
-                Console.WriteLine("\nCould not register, item was not added. Press Enter to add an item again");
+                Console.WriteLine("\nCould not register, item was not added. Press Enter to try again.");
                 Console.ReadLine();
                 break;
             }
